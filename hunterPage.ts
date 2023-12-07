fe80a8d1c9f7340d4137ab575363afae1b7a3f8b
Hunter <huntergerrard7@gmail.com>
2023-12-06 19:07:58 -0700

    updated 7:07pm

--- hunterPage.ts
+++ hunterPage.ts
@@ -1,2 +1,4 @@
-import{By}from 'selenium-webdriver'
-import{basePage}
+import{By}from 'selenium-webdriver';
+import{BasePage}from './basePage';
+
+export class Hunter extends BasePage{}
--- hunterTest.test.ts
+++ hunterTest.test.ts
@@ -1,4 +1,8 @@
-import{}from ""
+import{Hunter}from "./hunterPage"
 const hunter=new Hunter();
 
-test("can add stuff to git hub", async ()=>)
+test("can add stuff to git hub", async ()=>{
+
+
+    
+});
