798c35f61e02ae0697424a68dea883a93d3ea98b
Franciska Szente <frani.szente@gmail.com>
2023-12-13 18:52:40 -0800

    frani push

--- franiPage.ts
+++ franiPage.ts
@@ -4,6 +4,12 @@ import {BasePage} from './basePage';
 export class Frani extends BasePage {
     phones: By = By.xpath('(//span[@class="label font-body-md"])[1]');
     audio: By = By.xpath('(//a[@class="nav-box-link flex"])[2]');
+    tablets: By = By.xpath('(//a[@class="nav-box-link flex"])[3]');
+    gear: By = By.xpath('(//a[@class="nav-box-link flex"])[4]');
+    powerCables: By = By.xpath('(//a[@class="nav-box-link flex"])[5]');
+    casesProtection: By = By.xpath('(//a[@class="nav-box-link flex"])[6]');
+    bundles: By = By.xpath('(//a[@class="nav-box-link flex"])[7]');
+
 
     constructor(){
         super({url:'https://www.oneplus.com/us'})
--- franiTest.test.ts
+++ franiTest.test.ts
@@ -6,6 +6,11 @@ test("Click on the phones and audio", async () => {
     await page.navigate();
     await page.click(page.phones);
     await page.click(page.audio);
+    await page.click(page.tablets);
+    await page.click(page.gear);
+    await page.click(page.powerCables);
+    await page.click(page.powerCables);
+    await page.click(page.bundles);
     await page.driver.quit()
     
 })
