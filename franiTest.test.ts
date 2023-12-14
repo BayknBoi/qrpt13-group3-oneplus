4a223073f6cec549bad83f684e772f5e7686a7d3
Franciska Szente <frani.szente@gmail.com>
2023-12-13 20:47:49 -0800

    franiTest push

--- franiTest.test.ts
+++ franiTest.test.ts
@@ -6,10 +6,18 @@ test("Click on the phones and audio", async () => {
     await page.navigate();
     await page.click(page.phones);
     await page.click(page.audio);
+
+})
+
+test("Click on the tablets and gear", async () => {
     await page.click(page.tablets);
     await page.click(page.gear);
+    
+})
+
+test("Click on the power cables, cases and protection and bundles", async () => {
     await page.click(page.powerCables);
-    await page.click(page.powerCables);
+    await page.click(page.casesProtection);
     await page.click(page.bundles);
     await page.driver.quit()
     
